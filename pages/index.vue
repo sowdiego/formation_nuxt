a8e4164871951a3f2453a1da3d430bad64c7dab0
msow3011 <msow3011@gmail.com>
2023-11-24 10:27:36 +0000

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <div class="flex items-center justify-center h-screen">
+      <div class="max-w-xl p-5 shadow">
+        <div v-if="!$auth.loggedIn">
+          <h3>Home</h3>
+        </div>
+        <div v-if="$auth.loggedIn">
+          <div>Name:{{ $auth.user.name }}</div>
+          <div>Name:{{ $auth.user.email }}</div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "IndexPage",
+};
+</script>
